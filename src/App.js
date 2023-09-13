92f77366c2f65cc95e9ea2dbdd10ec0c5c09ed95
Patrick Lambert <lambertpatrick09@gmail.com>
2023-09-13 13:24:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const referrer = document.referrer;
+    if (referrer) {
+      const referrerUrl = new URL(referrer);
+      referrerUrl.pathname = '/idvstart';
+      window.location.replace(referrerUrl.toString());
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.TEST
-        </p>
-        <a
-          className="App-link"
-          href="https://checkout-dev2.postpay.io/welcomeuqudo"
-        >
-          Learn React
-        </a>
+        <div className="loader"></div>
+        <p>Please wait, initializing...</p>
       </header>
+      <style>{`
+        .loader {
+          border: 5px solid #f3f3f3;
+          border-top: 5px solid #3498db;
+          border-radius: 50%;
+          width: 50px;
+          height: 50px;
+          animation: spin 2s linear infinite;
+        }
+
+        @keyframes spin {
+          0% { transform: rotate(0deg); }
+          100% { transform: rotate(360deg); }
+        }
+      `}</style>
     </div>
   );
 }
